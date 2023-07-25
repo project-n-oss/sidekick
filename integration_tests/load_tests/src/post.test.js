e816e325d3991ffc77c8a50c0707469e02eea3a9
Kote <km@projectn.co>
2023-07-25 16:39:54 -0700

    feat: initial pass put

---
+++ integration_tests/load_tests/src/post.test.js
@@ -0,0 +1,60 @@
+import http from "k6/http";
+import { check } from "k6";
+import { AWSConfig, SignatureV4 } from "https://jslib.k6.io/aws/0.7.1/aws.js";
+
+export const options = {
+  stages: [
+    { duration: "5s", target: 100 },
+    { duration: "10s", target: 100 },
+    { duration: "5s", target: 0 },
+  ],
+};
+
+
+const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, BUCKET } =
+  process.env;
+
+  const awsConfig = new AWSConfig({
+  region: AWS_REGION,
+  accessKeyId: AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
+});
+
+
+const bucket = BUCKET;
+const key = "writes/1.csv";
+
+export default async function () {
+  const signer = new SignatureV4({
+    service: "s3",
+    region: awsConfig.region,
+    credentials: {
+      accessKeyId: awsConfig.accessKeyId,
+      secretAccessKey: awsConfig.secretAccessKey,
+    },
+  });
+
+  const signedRequest = signer.sign(
+    {
+      method: "PUT",
+      protocol: "http",
+      hostname: "localhost:7075",
+      path: `/${bucket}/${key}`,
+      headers: {},
+      uriEscapePath: false,
+      applyChecksum: true,
+    },
+    {
+      signingDate: new Date(),
+      signingService: "s3",
+    }
+  );
+
+  // Make a PUT request to the signed URL with the file in the body
+  const res = http.put(signedRequest.url, "Hello World!", {
+    headers: signedRequest.headers,
+  });
+  check(res, {
+    "is status 200": (r) => r.status === 200,
+  });
+}
