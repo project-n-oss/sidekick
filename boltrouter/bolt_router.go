e3002dd9665760d4cf911ddb11cba6ff2e0f0102
Kote <km@projectn.co>
2023-08-30 15:22:15 +0400

    feat: add platform dependent init; on gcp init an http client with google auth creds

--- boltrouter/bolt_router.go
+++ boltrouter/bolt_router.go
@@ -8,6 +8,8 @@ import (
 	"time"
 
 	"go.uber.org/zap"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
 // BoltRouter is used to find bolt endpoints and route a AWS call to the right endpoint.
@@ -16,6 +18,7 @@ type BoltRouter struct {
 
 	boltHttpClient     *http.Client
 	standardHttpClient *http.Client
+	gcpHttpClient      *http.Client
 	boltVars           *BoltVars
 	logger             *zap.Logger
 }
@@ -27,21 +30,56 @@ func NewBoltRouter(ctx context.Context, logger *zap.Logger, cfg Config) (*BoltRo
 		return nil, fmt.Errorf("could not get BoltVars: %w", err)
 	}
 
-	boltHttpClient := http.Client{
-		Timeout: time.Duration(90) * time.Second,
-	}
-
-	// custom transport is needed to allow certificate validation from bolt hostname
-	if tp, ok := http.DefaultTransport.(*http.Transport); ok {
-		customTransport := tp.Clone()
-		customTransport.TLSClientConfig = &tls.Config{
-			ServerName: boltVars.BoltHostname.Get(),
-		}
-		boltHttpClient.Transport = customTransport
-	}
+	var boltHttpClient http.Client
+	var gcpHttpClient http.Client
 
 	standardHttpClient := http.Client{
 		Timeout: time.Duration(90) * time.Second,
+	}
+
+	if cfg.CloudPlatform == "aws" {
+		boltHttpClient = http.Client{
+			Timeout: time.Duration(90) * time.Second,
+		}
+		// custom transport is needed to allow certificate validation from bolt hostname
+		if tp, ok := http.DefaultTransport.(*http.Transport); ok {
+			customTransport := tp.Clone()
+			customTransport.TLSClientConfig = &tls.Config{
+				ServerName: boltVars.BoltHostname.Get(),
+			}
+			boltHttpClient.Transport = customTransport
+		}
+	} else if cfg.CloudPlatform == "gcp" {
+		creds, err := google.FindDefaultCredentials(ctx, "https://www.googleapis.com/auth/devstorage.read_write")
+		if err != nil {
+			return nil, err
+		}
+		ts := oauth2.TokenSource(creds.TokenSource)
+		// custom transport is needed to allow certificate validation from bolt hostname
+		if tp, ok := http.DefaultTransport.(*http.Transport); ok {
+			customTransport := tp.Clone()
+			customTransport.TLSClientConfig = &tls.Config{
+				ServerName: boltVars.BoltHostname.Get(),
+			}
+			boltHttpClient = http.Client{
+				Timeout: time.Duration(90) * time.Second,
+				Transport: &oauth2.Transport{
+					Base:   customTransport,
+					Source: ts,
+				},
+			}
+		}
+
+		gcpHttpClient = http.Client{
+			Timeout: time.Duration(90) * time.Second,
+			Transport: &oauth2.Transport{
+				Base:   http.DefaultTransport,
+				Source: ts,
+			},
+		}
+
+	} else {
+		return nil, fmt.Errorf("invalid cloud platform: %s", cfg.CloudPlatform)
 	}
 
 	logger.Debug("config", zap.Any("config", cfg))
@@ -51,6 +89,7 @@ func NewBoltRouter(ctx context.Context, logger *zap.Logger, cfg Config) (*BoltRo
 
 		boltHttpClient:     &boltHttpClient,
 		standardHttpClient: &standardHttpClient,
+		gcpHttpClient:      &gcpHttpClient,
 		boltVars:           boltVars,
 	}
 
