e4466efa9e02d13f090a821f19c2950e9db649f6
Kote <km@projectn.co>
2023-11-27 16:00:29 -0800

    feat: return bad request if no auth header in request

--- api/error.go
+++ api/error.go
@@ -10,3 +10,8 @@ func (a *Api) InternalError(logger *zap.Logger, w http.ResponseWriter, err error
 	logger.Error("internal error", zap.Error(err))
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
+
+func (a *Api) BadRequest(logger *zap.Logger, w http.ResponseWriter, err error) {
+	logger.Error("bad request", zap.Error(err))
+	http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+}
